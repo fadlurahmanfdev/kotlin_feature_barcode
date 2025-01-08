ed79376d78bab9a98054422789c26839fb353606
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2025-01-08 17:52:27 +0700

    add ci cd

--- feature_barcode/build.gradle.kts
+++ feature_barcode/build.gradle.kts
@@ -1,6 +1,11 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("maven-publish")
+
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 android {
@@ -43,4 +48,37 @@ dependencies {
 
     api("com.fadlurahmanfdev:kotlin_feature_camera:0.3.0")
     api("com.google.mlkit:barcode-scanning:17.3.0")
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    coordinates("com.fadlurahmanfdev", "kotlin_feature_barcode", "0.0.1")
+
+    pom {
+        name.set("Kotlin Library Feature Barcode")
+        description.set("Library for handle barcode detection")
+        inceptionYear.set("2025")
+        url.set("https://github.com/fadlurahmanfdev/kotlin_feature_barcode/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("fadlurahmanfdev")
+                name.set("Taufik Fadlurahman Fajari")
+                url.set("https://github.com/fadlurahmanfdev/")
+            }
+        }
+        scm {
+            url.set("https://github.com/fadlurahmanfdev/kotlin_feature_barcode/")
+            connection.set("scm:git:git://github.com/fadlurahmanfdev/kotlin_feature_barcode.git")
+            developerConnection.set("scm:git:ssh://git@github.com/fadlurahmanfdev/kotlin_feature_barcode.git")
+        }
+    }
 }
